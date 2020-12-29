ab96eede92fa2a800c9b4ab85883287447f5dc55
ChenZhen <chenzhennba@gmail.com>
2020-12-30 00:35:54 +0800

    feat: 收集OC符号

--- OrderFileDemo/ViewController.m
+++ OrderFileDemo/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+#import "OrderFile.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [OrderFile parseSymbolToFileWithSuccess:^{
+            [self alertWithContent:@"符号收集成功"];
+    } fail:^{
+        [self alertWithContent:@"符号收集失败"];
+    }];
 }
 
+- (void)alertWithContent:(NSString *)content {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertController * alert = [UIAlertController
+                                    alertControllerWithTitle:@"Order file"
+                                    message:content
+                                    preferredStyle:UIAlertControllerStyleAlert];
+       UIAlertAction* yesButton = [UIAlertAction
+                                   actionWithTitle:@"好的"
+                                   style:UIAlertActionStyleDefault
+                                   handler:nil];
+
+       [alert addAction:yesButton];
+
+       [self presentViewController:alert animated:YES completion:nil];
+    });
+}
 
 @end
